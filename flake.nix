54b79a243a6be4c47be714aa417b54c4afe913fb
Pedro <pedro@local>
2026-09-07 18:34:15 -0300

    adiciona flakes e home-manager

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Config do Pedro";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-26.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.pedro = import ./home.nix;
+        }
+      ];
+    };
+  };
+}
---
+++ home.nix
@@ -0,0 +1,21 @@
+ { config, pkgs, ... }:
+
+{
+  home.username = "pedro";
+  home.homeDirectory = "/home/pedro";
+
+  home.stateVersion = "26.05";
+
+  home.packages = with pkgs; [
+    fastfetch
+    cava
+  ];
+
+  programs.git = {
+    enable = true;
+    userName = "Pedro";
+    userEmail = "Pedro@localhost";
+  };
+
+  programs.home-manager.enable = true;
+}
